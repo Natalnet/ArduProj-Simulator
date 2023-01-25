0a740480b6938a9ce585f4a82f16f370e317631e
marioxaxa <melonston@gmail.com>
2023-01-25 03:13:47 -0300

    Matriz concluida

--- src/helpers/connectivitysMatricesHelper.js
+++ src/helpers/connectivitysMatricesHelper.js
@@ -13,33 +13,29 @@ export function addConnectortToMatrix(componentId, connectorList, connectorMtx, 
         let row = Array(connectorList.length).fill(false)
         tempConnectorMtx = Array(connectorList.length).fill(row)
     } else {
-        // Caso ela não seja nula é copiado a 
-        console.log('entrou')
+        // Caso ela não seja nula é copiado a parte ja preenchida da matriz e adicionado a suas linhas valores falsos representando os novos connectors
+
         for (let index = 0; index < connectorMtx.length; index++) {
-            connectorList.forEach(element => {
-                tempConnectorMtx[index].push(false)
-            });
+
+            let inicialArray = tempConnectorMtx[index]
+            let finalArray = Array(connectorList.length).fill(false)
+            tempConnectorMtx[index] = [...inicialArray, ...finalArray]
+
         }
-        console.log('check 1')
-        console.log(connectorMtx.length)
-        console.log(connectorList.length)
-        for (let index = connectorMtx.length; index < (connectorMtx.length + connectorList.length); index++) {
-            let row = Array(connectorMtx.length + connectorList.length).fill(false)
-            //! ERRO AQUI GERANDO LOOP INFINITO
+
+        // E depois é adicionado as colunas desses novos connectors a matriz
+
+        let maxLength = (connectorMtx.length + connectorList.length)
+        for (let index = connectorMtx.length; index < maxLength; index++ ) {
+            let row = Array(maxLength).fill(false)
             tempConnectorMtx[index] = row
-            //!
-            console.log(index)
         }
-        console.log('check 2')
     }
 
-
-
+    // Por fim é feito o array do map adicionando os connectors novos a um array ja existente
     connectorList.forEach(connector => {
         tempConnectorMtxMap.push(`${connector.svgId}/${componentId}`)
     })
-
-    console.log(tempConnectorMtx)
 
     return ({ matrix: tempConnectorMtx, maping: tempConnectorMtxMap })
 
