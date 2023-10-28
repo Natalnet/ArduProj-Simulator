22938a35f4317123739f0869ec33a2347a289a61
julio <julio@solarz.com.br>
2023-10-28 20:13:55 -0300

    feat. adicionaod teste de digitalRead no default code.

--- src/helpers/default_code.js
+++ src/helpers/default_code.js
@@ -2,12 +2,16 @@ export default `
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(10, INPUT);
 }
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  int readVal = digitalRead(10);
+  digitalWrite(12, readVal);
   delay(1000);                       // wait for a second
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+  delay(1000);                        // wait for a second
 }
 `;
